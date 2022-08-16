805700554dcb51a00afd60485b01a79975ad2565
chezhe <chezhe@hey.com>
2022-08-16 18:02:10 +0800

    fix metrics not found bug

--- src/views/Appchain/MyNode/InstanceInfoModal.tsx
+++ src/views/Appchain/MyNode/InstanceInfoModal.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react"
 
 import {
   Box,
@@ -8,28 +8,31 @@ import {
   Icon,
   Progress,
   Divider,
-  HStack
-} from '@chakra-ui/react';
+  HStack,
+} from "@chakra-ui/react"
 
-import { BaseModal } from 'components';
-import { FiCpu } from 'react-icons/fi';
-import { FaMemory } from 'react-icons/fa';
-import { GrStorage } from 'react-icons/gr';
+import { BaseModal } from "components"
+import { FiCpu } from "react-icons/fi"
+import { FaMemory } from "react-icons/fa"
+import { GrStorage } from "react-icons/gr"
 
 type Props = {
-  isOpen: boolean;
-  onClose: VoidFunction;
-  metrics: any;
+  isOpen: boolean
+  onClose: VoidFunction
+  metrics: any
 }
 
-export const InstanceInfoModal: React.FC<Props> = ({ metrics, isOpen, onClose }) => {
-  console.log(metrics);
+export const InstanceInfoModal: React.FC<Props> = ({
+  metrics,
+  isOpen,
+  onClose,
+}) => {
+  if (!metrics || metrics?.Code === "NotFoundError") {
+    return null
+  }
 
   return (
-    <BaseModal 
-      isOpen={isOpen} 
-      onClose={onClose}
-      title="Instance Info">
+    <BaseModal isOpen={isOpen} onClose={onClose} title="Instance Info">
       <Box p={4}>
         <Flex justifyContent="space-between">
           <HStack>
@@ -37,18 +40,25 @@ export const InstanceInfoModal: React.FC<Props> = ({ metrics, isOpen, onClose })
             <Text>CPU(s)</Text>
           </HStack>
           <VStack spacing={0}>
-            {
-              metrics ? Object.keys(metrics?.cpu).map((c: any, idx: number) => (
-                <HStack key={`cpu-${idx}`}>
-                  <Box w="160px">
-                    <Progress value={metrics.cpu[c].percentage * 100} colorScheme={
-                      metrics.cpu[c].percentage > 0.9 ? 'red': 'blue'
-                    } size="sm" borderRadius="lg" />
-                  </Box>
-                  <Text fontSize="xs" color="gray">{(metrics.cpu[c].percentage * 100).toFixed(2)}%</Text>
-                </HStack>
-              )) : null
-            }
+            {metrics
+              ? Object.keys(metrics?.cpu).map((c: any, idx: number) => (
+                  <HStack key={`cpu-${idx}`}>
+                    <Box w="160px">
+                      <Progress
+                        value={metrics.cpu[c].percentage * 100}
+                        colorScheme={
+                          metrics.cpu[c].percentage > 0.9 ? "red" : "blue"
+                        }
+                        size="sm"
+                        borderRadius="lg"
+                      />
+                    </Box>
+                    <Text fontSize="xs" color="gray">
+                      {(metrics.cpu[c].percentage * 100).toFixed(2)}%
+                    </Text>
+                  </HStack>
+                ))
+              : null}
           </VStack>
         </Flex>
         <Divider mt={4} mb={4} />
@@ -59,11 +69,16 @@ export const InstanceInfoModal: React.FC<Props> = ({ metrics, isOpen, onClose })
           </HStack>
           <HStack>
             <Box w="160px">
-              <Progress value={metrics?.memory.percentage * 100} colorScheme={
-                metrics?.memory.percentage > 0.9 ? 'red': 'blue'
-              } size="sm" borderRadius="lg" />
+              <Progress
+                value={metrics?.memory.percentage * 100}
+                colorScheme={metrics?.memory.percentage > 0.9 ? "red" : "blue"}
+                size="sm"
+                borderRadius="lg"
+              />
             </Box>
-            <Text fontSize="xs" color="gray">{(metrics?.memory.percentage * 100).toFixed(2)}%</Text>
+            <Text fontSize="xs" color="gray">
+              {(metrics?.memory.percentage * 100).toFixed(2)}%
+            </Text>
           </HStack>
         </Flex>
         <Divider mt={4} mb={4} />
@@ -74,14 +89,21 @@ export const InstanceInfoModal: React.FC<Props> = ({ metrics, isOpen, onClose })
           </HStack>
           <HStack>
             <Box w="160px">
-              <Progress value={metrics?.filesystem.percentage * 100} colorScheme={
-                metrics?.filesystem.percentage > 0.9 ? 'red': 'blue'
-              } size="sm" borderRadius="lg" />
+              <Progress
+                value={metrics?.filesystem.percentage * 100}
+                colorScheme={
+                  metrics?.filesystem.percentage > 0.9 ? "red" : "blue"
+                }
+                size="sm"
+                borderRadius="lg"
+              />
             </Box>
-            <Text fontSize="xs" color="gray">{(metrics?.filesystem.percentage * 100).toFixed(2)}%</Text>
+            <Text fontSize="xs" color="gray">
+              {(metrics?.filesystem.percentage * 100).toFixed(2)}%
+            </Text>
           </HStack>
         </Flex>
       </Box>
     </BaseModal>
-  );
+  )
 }
