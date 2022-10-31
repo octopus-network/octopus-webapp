4a00454f4169b9d4a9446a2dfea96753420bf574
chezhe <chezhe@hey.com>
2022-10-31 11:04:17 +0800

    optimize: getDelegatedValidators

--- src/utils/appchain.ts
+++ src/utils/appchain.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 import { NetworkConfig, RewardHistory } from "types";
 import { DecimalUtil, ZERO_DECIMAL } from "./decimal";
 import { API_HOST } from "config";
+import posthog from "posthog-js";
 
 export const getUnbondedValidators = async (
   networkConfig: NetworkConfig,
@@ -30,11 +31,55 @@ export const getUnbondedValidators = async (
   }
 };
 
+export const getRedelegateHistory = async (
+  networkConfig: NetworkConfig,
+  appchain_anchor: string,
+  delegatorId: string
+) => {
+  const arr: string[] = [];
+  let success = true;
+  try {
+    const redelegateRes = await axios.post(
+      `${networkConfig?.near.restApiUrl}/explorer`,
+      {
+        user: "public_readonly",
+        host: `${networkConfig?.near.networkId}.db.explorer.indexer.near.dev`,
+        database: `${networkConfig?.near.networkId}_explorer`,
+        password: "nearprotocol",
+        port: 5432,
+        parameters: [appchain_anchor, delegatorId],
+        query: `
+          SELECT * FROM public.action_receipt_actions 
+          WHERE receipt_receiver_account_id = $1
+          AND args->>'method_name' = 'change_delegated_validator'
+          AND receipt_predecessor_account_id = $2
+          LIMIT 100;
+        `,
+      }
+    );
+
+    redelegateRes.data.forEach((t: any) => {
+      if (t.args.args_json) {
+        arr.push(t.args.args_json.new_validator_id);
+        arr.push(t.args.args_json.old_validator_id);
+      }
+    });
+  } catch (error) {
+    success = false;
+    posthog.capture("get_redelegate_error");
+  }
+  return {
+    success,
+    arr: Array.from(new Set(arr)),
+  };
+};
+
 export const getDelegatedValidators = async (
   networkConfig: NetworkConfig,
   appchain_anchor: string,
   delegatorId: string
 ): Promise<string[]> => {
+  let arr = [];
   try {
     const registerRes = await axios.post(
       `${networkConfig?.near.restApiUrl}/explorer`,
@@ -59,27 +104,7 @@ export const getDelegatedValidators = async (
         `,
       }
     );
-
-    const redelegateRes = await axios.post(
-      `${networkConfig?.near.restApiUrl}/explorer`,
-      {
-        user: "public_readonly",
-        host: `${networkConfig?.near.networkId}.db.explorer.indexer.near.dev`,
-        database: `${networkConfig?.near.networkId}_explorer`,
-        password: "nearprotocol",
-        port: 5432,
-        parameters: [appchain_anchor, delegatorId],
-        query: `
-          SELECT * FROM public.action_receipt_actions 
-          WHERE receipt_receiver_account_id = $1
-          AND args->>'method_name' = 'change_delegated_validator'
-          AND receipt_predecessor_account_id = $2
-          LIMIT 100;
-        `,
-      }
-    );
-
-    const registerArr = registerRes.data.map((r: any) => {
+    arr = registerRes.data.map((r: any) => {
       try {
         const obj = JSON.parse(
           decodeURIComponent(r.args.args_json.msg.replace(/\\/g, ""))
@@ -89,17 +114,26 @@ export const getDelegatedValidators = async (
         return "";
       }
     });
-    redelegateRes.data.forEach((t: any) => {
-      if (t.args.args_json) {
-        registerArr.push(t.args.args_json.new_validator_id);
-        registerArr.push(t.args.args_json.old_validator_id);
-      }
-    });
-
-    return Array.from(new Set(registerArr.filter((t: string) => t !== "")));
   } catch (error) {
-    return [];
+    posthog.capture("get_registed_validator_error");
   }
+  const { success, arr: redelegates } = await getRedelegateHistory(
+    networkConfig,
+    appchain_anchor,
+    delegatorId
+  );
+  if (success) {
+    arr = arr.concat(redelegates);
+  } else {
+    const { arr: redelegates } = await getRedelegateHistory(
+      networkConfig,
+      appchain_anchor,
+      delegatorId
+    );
+    arr = arr.concat(redelegates);
+  }
+
+  return Array.from(new Set(arr.filter((t: string) => t !== "")));
 };
 
 export const calcUnwithdrawnReward = (
